e9b7db92c7b77d6205a7f6bc389d89d414b199ac
Vinicius Dias <153852195+vini7kk@users.noreply.github.com>
2024-05-25 11:57:38 -0300

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,3 +1,4 @@
+//Efeito hover no texto
 document.addEventListener("DOMContentLoaded", () => {
     const heading = document.querySelector(".text");
     const words = heading.textContent.split(' ').map(word => {
@@ -20,3 +21,16 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 });
 
+
+//Menu Hamburguer JavaScript settings
+const hamburger = document.querySelector(".hamburger");
+const nav = document.querySelector(".nav");
+
+hamburger.addEventListener("click", () => {
+    console.log("Botão de hambúrguer clicado");
+    
+    nav.classList.toggle("active");
+    hamburger.classList.toggle("active");
+
+    console.log("Classe 'active' adicionada/removida do menu de navegação e do botão de hambúrguer");
+});
